c85ae01ada9abe557a6ea380954a0f97580a896d
Brian Lee <hclee@suprema.co.kr>
2019-08-21 08:29:03 +0900

    Added a function to test the library.

--- app/src/main/java/com/supremainc/sfm_sdk_android/MainActivity.java
+++ app/src/main/java/com/supremainc/sfm_sdk_android/MainActivity.java
@@ -3,11 +3,17 @@ package com.supremainc.sfm_sdk_android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.supremainc.sfm_sdk.SFM_SDK_ANDROID;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SFM_SDK_ANDROID sdk = new SFM_SDK_ANDROID();
+
+        sdk.GetSDKVersion();
     }
 }
---
+++ sfm_sdk/src/main/java/com/supremainc/sfm_sdk/SFM_SDK_ANDROID.java
@@ -0,0 +1,27 @@
+package com.supremainc.sfm_sdk;
+
+import android.util.Log;
+
+public class SFM_SDK_ANDROID {
+
+    /**
+     * Constructor
+     */
+    public SFM_SDK_ANDROID(){
+
+
+    }
+
+    public static void GetSDKVersion()
+    {
+        Log.i("[INFO]", "Test Version");
+    }
+
+    /**
+     * Destructor
+     */
+    public void finalize()
+    {
+
+    }
+}
